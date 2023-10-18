70207c408a1c7e77e75c93b53f43747662a0918f
Chirukaru36 <kpoojashree470@gmail.com>
2023-10-18 20:17:07 +0530

    Add project files.

---
+++ ASSIGNMENT 1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASSIGNMENT_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Smartphone> smartphones = new List<Smartphone>();
+        
+            smartphones.Add(new Smartphone { IMEINumber = "12345", Name = "iPhone 12", Model = "A14 Bionic", Price = 799.99 });
+            smartphones.Add(new Smartphone { IMEINumber = "67890", Name = "Samsung Galaxy S21", Model = "Exynos 2100", Price = 899.99 });
+            smartphones.Add(new Smartphone { IMEINumber = "13579", Name = "Google Pixel 5", Model = "Snapdragon 765G", Price = 699.99 });
+            smartphones.Add(new Smartphone { IMEINumber = "24680", Name = "OnePlus 9", Model = "Snapdragon 888", Price = 749.99 });
+            smartphones.Add(new Smartphone { IMEINumber = "98765", Name = "Xiaomi Mi 11", Model = "Snapdragon 888", Price = 699.99 });
+        
+
+            Console.WriteLine("Before sorting:");
+            foreach (var phone in smartphones)
+            {
+                Console.WriteLine($"{phone.Name} - {phone.Model}");
+            }
+
+            smartphones.Sort();
+
+            Console.WriteLine("\nAfter sorting by name using IComparable:");
+            foreach (var phone in smartphones)
+            {
+                Console.WriteLine($"{phone.Name} ,{phone.Model}");
+            }
+
+            smartphones.Sort(new SmartphoneNameComparer());
+
+            Console.WriteLine("\nAfter sorting by name using IComparer:");
+            foreach (var phone in smartphones)
+            {
+                Console.WriteLine($"{phone.Name} ,{phone.Model}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ ASSIGNMENT 1/Smartphone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASSIGNMENT_1
+{
+    class Smartphone:IComparable<Smartphone>
+    {
+        public string IMEINumber {  get; set; }
+        public string Name { get; set; }
+
+        public string Model { get; set; }
+
+        public double Price {  get; set; }
+
+        public int CompareTo(Smartphone other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            return string.Compare(this.Name, other.Name, StringComparison.Ordinal);
+        }
+    }
+
+    class SmartphoneNameComparer : IComparer<Smartphone>
+    {
+        public int Compare(Smartphone x, Smartphone y)
+        {
+            if (x == null || y == null)
+            {
+                throw new ArgumentException("Cannot compare null objects.");
+            }
+            return string.Compare(x.Name, y.Name, StringComparison.Ordinal);
+        }
+    }
+}
